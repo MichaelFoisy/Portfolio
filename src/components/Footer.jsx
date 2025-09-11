c21dccad4a7437ef172a8ffbf6369353b93ba289
Michael Foisy <99748474+MichaelFoisy@users.noreply.github.com>
2025-09-11 12:18:01 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className="bg-slate-900 text-white py-4 text-center">
-    <p>&copy; 2025 Michael  | Built with React</p>
+    <p>&copy; 2025 Michael Foisy | Built with React</p>
   </footer>
   )
 }
